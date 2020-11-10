125d310bebb4f04d73c2959148d8e69147bc7c33
NickyEXE <dovern42@gmail.com>
2020-11-10 17:49:02 -0500

    removes add message capability

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,4 @@ class MessagesController < ApplicationController
     render json: @messages
   end
 
-  def create
-    @message = Message.create(message_params)
-    render json: @message
-  end
-
-  def message_params
-    params.permit(:username, :content)
-  end
 end
